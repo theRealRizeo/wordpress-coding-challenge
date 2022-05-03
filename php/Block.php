8a809e74999671b322f4224d4ec4971d011fd711
Paul Kevin <paultitude@gmail.com>
2022-05-03 18:36:47 +0300

    Code standards and sanitization
    Add text escapes to strings output on HTML. Fix queries to limit number of items returned. Fix indentation. Fix array references.

--- php/Block.php
+++ php/Block.php
@@ -63,63 +63,73 @@ class Block {
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
-		$class_name = $attributes['className'];
+		$post_types      = get_post_types( array( 'public' => true ) );
+		$class_name      = sanitize_html_class( $attributes['className'] );
+		$current_post_id = get_the_ID();
+
 		ob_start();
 
 		?>
-        <div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
+		<div class="<?php echo esc_attr( $class_name ); ?>">
+			<h2><?php esc_html_e( 'Post Counts', 'site-counts' ); ?></h2>
 			<ul>
+				<?php
+				foreach ( $post_types as $post_type_slug ) :
+					$post_type_object = get_post_type_object( $post_type_slug );
+					$posts_count      = wp_count_posts( $post_type_slug );
+					?>
+					<li>
+						<?php
+						/* Translators: %1$d The total count, %2$s The post type label. */
+						printf( esc_html__( 'There are %1$d %2$s.', 'site-counts' ), esc_attr( $posts_count->publish ), esc_html( $post_type_object->labels->name ) );
+						?>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+			<?php if ( $current_post_id ) : ?>
+				<p>
+					<?php
+					/* Translators: %d The current post id. */
+					printf( esc_html__( 'The current post ID is %d.', 'site-counts' ), esc_attr( $current_post_id ) );
+					?>
+				</p>
+			<?php endif; ?>
 			<?php
-			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-                );
-
-				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
-
-			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
+			$query = new WP_Query(
+				array(
+					'post_type'        => array( 'post', 'page' ),
+					'post_status'      => 'any',
+					'date_query'       => array(
+						array(
+							'hour'     => 9,
+							'compare'  => '>=',
+						),
+						array(
+							'hour'     => 17,
+							'compare'  => '<=',
+						),
 					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-			));
+					'tag'              => 'foo',
+					'category_name'    => 'baz',
+					'posts_per_page '  => 5,
+					'suppress_filters' => false,
+				)
+			);
 
 			if ( $query->have_posts() ) :
 				?>
-				 <h2>5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
-
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
-				endforeach;
-			endif;
-		 	?>
-			</ul>
+				<h2>
+					<?php
+					/* Translators: %1$s The tag name, %2$s The category name. */
+					printf( esc_html__( '5 posts with the tag of %1$s and the category of %2$s', 'site-counts' ), 'foo', 'baz' );
+					?>
+				</h2>
+				<ul>
+					<?php foreach ( $query->posts as $post ) : ?>
+						<li><?php echo esc_html( $post->post_title ); ?></li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
 		</div>
 		<?php
 
